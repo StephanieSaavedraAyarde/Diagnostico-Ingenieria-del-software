dc5dbbb6f0bac300a73d0ed83d98e6e4a724c4d8
StephanieSaavedraAyarde <stephanie.saavedra@ucb.edu.bo>
2021-09-27 03:26:28 -0400

    Auth Version 1

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -19,4 +19,17 @@ export class LoginComponent implements OnInit {
   constructor(public dialog: MatDialog, private http: HttpClient) {}
 
   ngOnInit() {}
+
+  Verify(data): void {
+    if (!data) {
+      this.http
+        .post<any>('http://localhost:3000/auth', {
+          username: data.username,
+          password: data.password,
+        })
+        .subscribe((data) => {
+          console.log('SE ESTA VERIFICANDO EN EL BACKEND');
+        });
+    }
+  }
 }
